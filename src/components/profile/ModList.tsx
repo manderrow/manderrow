99a163b6a9e10dc617979adb6d025ab1dbb402d2
Michael Pfaff <michael@pfaff.dev>
2024-12-01 23:51:46 -0500

    Improve CSS for mod list layout

--- src/components/profile/ModList.tsx
+++ src/components/profile/ModList.tsx
@@ -24,50 +24,52 @@ export type Fetcher = (page: number) => Promise<ModAndVersion[]>;
 function ModListLeft({ mods, selectedMod: [selectedMod, setSelectedMod] }: { mods: Fetcher, selectedMod: Signal<ModAndVersion | undefined> }) {
   const [paginatedMods, infiniteScrollLoader, { end }] = createInfiniteScroll(mods);
 
-  return <ol class={styles.modList}>
-    <For each={paginatedMods()}>
-      {mod => <li classList={{ [styles.selected]: selectedMod() === mod }}>
-        <button on:click={() => setSelectedMod(selectedMod() === mod ? undefined : mod)}>
-          <img class={styles.icon} src={mod.mod.versions[0].icon} />
-          <div class={styles.split}>
-            <div class={styles.left}>
-              <div>
-                <span class={styles.name}>{mod.mod.name}</span> <span class={styles.version}>v
-                  <Show when={mod.version} fallback={mod.mod.versions[0].version_number}>
-                    {version => version()}
-                  </Show>
-                </span>
+  return <div class={styles.scrollOuter}>
+    <ol class={`${styles.modList} ${styles.scrollInner}`}>
+      <For each={paginatedMods()}>
+        {mod => <li classList={{ [styles.selected]: selectedMod() === mod }}>
+          <button on:click={() => setSelectedMod(selectedMod() === mod ? undefined : mod)}>
+            <img class={styles.icon} src={mod.mod.versions[0].icon} />
+            <div class={styles.split}>
+              <div class={styles.left}>
+                <div>
+                  <span class={styles.name}>{mod.mod.name}</span> <span class={styles.version}>v
+                    <Show when={mod.version} fallback={mod.mod.versions[0].version_number}>
+                      {version => version()}
+                    </Show>
+                  </span>
+                </div>
+                <div class={styles.owner}><span class={styles.label}>@</span><span class={styles.value}>{mod.mod.owner}</span></div>
+                <div class={styles.categories}>
+                  <For each={mod.mod.categories}>
+                    {category => <div>{category}</div>}
+                  </For>
+                </div>
               </div>
-              <div class={styles.owner}><span class={styles.label}>@</span><span class={styles.value}>{mod.mod.owner}</span></div>
-              <div class={styles.categories}>
-                <For each={mod.mod.categories}>
-                  {category => <div>{category}</div>}
-                </For>
+              <div class={styles.right}>
+                <p class={styles.downloads}><span class={styles.label}>Downloads: </span><span class={styles.value}>{mod.mod.versions[0].downloads ?? '0'}</span></p>
               </div>
             </div>
-            <div class={styles.right}>
-              <p class={styles.downloads}><span class={styles.label}>Downloads: </span><span class={styles.value}>{mod.mod.versions[0].downloads ?? '0'}</span></p>
-            </div>
-          </div>
-        </button>
-      </li>}
-    </For>
-    <Show when={!end()}>
-      <li use:infiniteScrollLoader>Loading...</li>
-    </Show>
-  </ol>;
+          </button>
+        </li>}
+      </For>
+      <Show when={!end()}>
+        <li use:infiniteScrollLoader>Loading...</li>
+      </Show>
+    </ol>
+  </div>;
 }
 
 export default function ModList(props: { mods: Fetcher }) {
   const [selectedMod, setSelectedMod] = createSignal<ModAndVersion>();
 
   return <div class={styles.modListAndView}>
-    <div class={styles.inner}>
-      <Show when={props.mods} keyed>
-        {mods => <ModListLeft mods={mods} selectedMod={[selectedMod, setSelectedMod]} />}
-      </Show>
-      <Show when={selectedMod()}>
-        {mod => <div class={styles.modView}>
+    <Show when={props.mods} keyed>
+      {mods => <ModListLeft mods={mods} selectedMod={[selectedMod, setSelectedMod]} />}
+    </Show>
+    <Show when={selectedMod()}>
+      {mod => <div class={styles.scrollOuter}>
+        <div class={`${styles.modView} ${styles.scrollInner}`}>
           <h2 class={styles.name}>{mod().mod.name}</h2>
           <p class={styles.description}>{mod().mod.versions[0].description}</p>
 
@@ -89,8 +91,8 @@ export default function ModList(props: { mods: Fetcher }) {
               }}
             </For>
           </ol>
-        </div>}
-      </Show>
-    </div>
+        </div>
+      </div>}
+    </Show>
   </div>;
 }
