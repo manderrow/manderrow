7b6ea4757b4cfcdc8f1abde0ba64de346fe38145
Jack <canadajackhuang@gmail.com>
2024-12-02 23:15:48 -0500

    Add error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,24 @@ import { Route, Router } from "@solidjs/router";
 
 import "./App.css";
 
-import { Show } from "solid-js";
+import { ErrorBoundary, Show } from "solid-js";
+
 import { gamesResource } from "./globals";
-import Error from "./pages/error/Error";
+
+import ErrorPage from "./pages/error/Error";
 import GameSelect from "./pages/game_select/GameSelect";
 import Profile from "./pages/profile/Profile";
 
 export default function App() {
   return (
-    <Show when={gamesResource.latest}>
-      <Router>
-        <Route path="/" component={GameSelect} />
-        <Route path="/profile/:gameId/:profileId?" component={Profile} />
-        <Route path="*404" component={Error} />
-      </Router>
-    </Show>
+    <ErrorBoundary fallback={ErrorPage}>
+      <Show when={gamesResource.latest != null}>
+        <Router>
+          <Route path="/" component={GameSelect} />
+          <Route path="/profile/:gameId/:profileId?" component={Profile} />
+          <Route path="*path" component={ErrorPage} />
+        </Router>
+      </Show>
+    </ErrorBoundary>
   );
 }
--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,7 +1,18 @@
+import { A, useParams } from "@solidjs/router";
+import { Show } from "solid-js";
+
 export default function Error() {
+  const params = useParams();
+  const is404 = () => params.path != null;
+
   return (
     <main>
       <h1>Error</h1>
+      <Show when={is404()} fallback={<p>A fatal error has occurred...</p>}>
+        <p>This page does not exist.</p>
+        <p>{params.path}</p>
+        <A href="/">Go back home</A>
+      </Show>
     </main>
   );
 }
