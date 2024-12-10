dbcdc74293faab93998bbc5faced8dafcc253b2b
Michael Pfaff <michael@pfaff.dev>
2024-12-10 10:44:19 -0500

    Add Dialog component

---
+++ src/components/Dialog.tsx
@@ -0,0 +1,12 @@
+import { JSX } from "solid-js";
+import { Portal } from "solid-js/web";
+
+import styles from "./Dialog.module.css";
+
+export default function Dialog(props: { children: JSX.Element }) {
+  return (
+    <Portal>
+      <div class={styles.dialog}>{props.children}</div>
+    </Portal>
+  );
+}
