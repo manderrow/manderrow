899b6224c802b102d3d1e981e1fd1bef9a0a624b
Jack <canadajackhuang@gmail.com>
2024-12-11 16:29:00 -0500

    Basic 404 page styling

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,18 +1,18 @@
 import { A, useParams } from "@solidjs/router";
-import { Show } from "solid-js";
+
+import styles from "./Error.module.css";
 
 export default function Error() {
   const params = useParams();
-  const is404 = () => params.path != null;
 
   return (
-    <main>
-      <h1>Error</h1>
-      <Show when={is404()} fallback={<p>A fatal error has occurred...</p>}>
-        <p>This page does not exist.</p>
-        <p>{params.path}</p>
-        <A href="/">Go back home</A>
-      </Show>
+    <main class={styles.error}>
+      <h1 class={styles.error_heading}>404</h1>
+      <p class={styles.error_path}>/{params.path}</p>
+      <p class={styles.error_msg}>There is nothing on this page. You somehow got here...</p>
+      <A href="/" tabindex="-1">
+        <button>Go back home</button>
+      </A>
     </main>
   );
 }
