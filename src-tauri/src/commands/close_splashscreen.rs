b3b2aa5df22f16c417970f24b39a81535e483905
Jack <canadajackhuang@gmail.com>
2025-01-22 21:33:59 -0500

    Delete old commented code

--- src-tauri/src/commands/close_splashscreen.rs
+++ src-tauri/src/commands/close_splashscreen.rs
@@ -3,8 +3,6 @@ use tauri::{Manager, Window};
 #[tauri::command]
 pub async fn close_splashscreen(window: Window) {
     let main_window = window.get_webview_window("main").expect("no windows?");
-    // main_window.center().unwrap();
-    // main_window.maximize().unwrap();
     main_window.show().unwrap();
 
     let splashscreen_window = window.get_webview_window("splashscreen");
