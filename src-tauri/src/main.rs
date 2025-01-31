6b55f9e97494454cc26e6d3c7405261df0c8ca45
Jack <canadajackhuang@gmail.com>
2025-01-31 00:56:37 -0500

    Always remove console window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,4 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![windows_subsystem = "windows"]
 #![deny(unused_must_use)]
 
 fn main() -> anyhow::Result<()> {
