039bf4059c8a05014fbb2ae253dd153185d6a871
Jack <canadajackhuang@gmail.com>
2025-02-12 15:48:43 -0500

    Add external link handler

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Route, Router } from "@solidjs/router";
 import { invoke } from "@tauri-apps/api/core";
 import { platform } from "@tauri-apps/plugin-os";
+import { open } from "@tauri-apps/plugin-shell";
 import { Resource, Show, createEffect, createSignal, onCleanup, onMount } from "solid-js";
 
 import { gamesPopularityResource, gamesResource } from "./globals";
@@ -17,6 +18,14 @@ const resources: Resource<any>[] = [gamesResource, gamesPopularityResource];
 export default function App() {
   const [loaded, setLoaded] = createSignal(false);
   const [ready, setReady] = createSignal(false);
+
+  function onLinkClick(event: MouseEvent) {
+    if (!(event.target instanceof HTMLAnchorElement)) return;
+    if (event.target.href.startsWith(`${window.location.protocol}//${window.location.host}`)) return;
+
+    event.preventDefault();
+    open(event.target.href);
+  }
 
   onMount(async () => {
     // 64px taken from the title on game select screen
@@ -38,10 +47,13 @@ export default function App() {
   onMount(async () => {
     const platformName = platform();
     document.body.dataset.webview = platformName === "macos" || platformName === "ios" ? "safari" : platformName;
+
+    document.addEventListener("click", onLinkClick);
   });
 
   onCleanup(() => {
     delete document.body.dataset.webview;
+    document.removeEventListener("click", onLinkClick);
   });
 
   return (
