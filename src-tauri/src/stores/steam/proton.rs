71057acac0b4ae12cf7a1a32478797bc199a6272
Michael Pfaff <michael@pfaff.dev>
2025-04-13 00:40:46 -0400

    just fmt

--- src-tauri/src/stores/steam/proton.rs
+++ src-tauri/src/stores/steam/proton.rs
@@ -3,7 +3,7 @@ use std::ops::Range;
 use anyhow::{bail, Result};
 use slog::{debug, trace};
 
-use super::paths::{resolve_steam_app_compat_data_directory, resolve_app_install_directory};
+use super::paths::{resolve_app_install_directory, resolve_steam_app_compat_data_directory};
 
 pub async fn uses_proton(log: &slog::Logger, game_id: &str) -> Result<bool> {
     if cfg!(target_os = "linux") {
