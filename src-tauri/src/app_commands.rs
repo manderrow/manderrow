85cf2c04db5eb24018deb919ad6db8af087d2048
Michael Pfaff <michael@pfaff.dev>
2025-07-24 15:09:19 -0400

    Semi-fix titlebar on macOS

--- src-tauri/src/app_commands.rs
+++ src-tauri/src/app_commands.rs
@@ -1,8 +1,66 @@
 use std::ffi::OsString;
 
-use tauri::{AppHandle, Manager};
+use anyhow::Context;
+use tauri::{AppHandle, Manager, Window};
 
 use crate::CommandError;
+
+#[tauri::command]
+pub async fn close(window: Window) -> Result<(), CommandError> {
+    window.close().context("Failed to close window")?;
+    Ok(())
+}
+
+fn is_maximized_impl(window: &Window) -> Result<bool, CommandError> {
+    Ok(if cfg!(target_os = "macos") {
+        window.is_fullscreen()
+    } else {
+        window.is_maximized()
+    }
+    .context("Failed to check if window is maximized")?)
+}
+
+#[tauri::command]
+pub async fn is_maximized(window: Window) -> Result<bool, CommandError> {
+    is_maximized_impl(&window)
+}
+
+#[tauri::command]
+pub async fn minimize(window: Window) -> Result<(), CommandError> {
+    window.minimize().context("Failed to minimize window")?;
+    Ok(())
+}
+
+#[tauri::command]
+pub async fn set_maximized(
+    window: Window,
+    desired_state: Option<bool>,
+) -> Result<(), CommandError> {
+    let desired_state = match desired_state {
+        Some(b) => b,
+        None => !is_maximized_impl(&window)?,
+    };
+    if cfg!(target_os = "macos") {
+        window
+            .set_fullscreen(desired_state)
+            .context("Failed to set_fullscreen window")?;
+    } else {
+        if desired_state {
+            window.maximize().context("Failed to maximize window")?;
+        } else {
+            window.unmaximize().context("Failed to unmaximize window")?;
+        }
+    }
+    Ok(())
+}
+
+#[tauri::command]
+pub async fn start_dragging(window: Window) -> Result<(), CommandError> {
+    window
+        .start_dragging()
+        .context("Failed to start dragging window")?;
+    Ok(())
+}
 
 #[tauri::command]
 pub async fn relaunch(app: AppHandle) -> Result<(), CommandError> {
--- src/api/app.ts
+++ src/api/app.ts
@@ -1,6 +1,27 @@
 import { invoke } from "@tauri-apps/api/core";
 import { wrapInvoke } from "../api";
 
+export async function close(): Promise<never> {
+  await wrapInvoke(() => invoke("close"));
+  throw Error("this should be unreachable");
+}
+
+export function isMaximized(): Promise<boolean> {
+  return wrapInvoke(() => invoke("is_maximized"));
+}
+
+export function minimize(): Promise<void> {
+  return wrapInvoke(() => invoke("minimize"));
+}
+
 export function relaunch(): Promise<never> {
   return wrapInvoke(() => invoke("relaunch"));
+}
+
+export function setMaximized(desiredState?: boolean): Promise<never> {
+  return wrapInvoke(() => invoke("set_maximized", { desiredState }));
+}
+
+export function startDragging(): Promise<void> {
+  return wrapInvoke(() => invoke("start_dragging"));
 }
