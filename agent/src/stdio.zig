manderrow/manderrow

commit 44a10168a319bdea292d5763a55de1c23e1a4530

agent: Update Zig to 0.15.0-dev.1092+d772c0627

agent/src/stdio.zig
@@ -80,35 +80,39 @@ fn GetStdHandlePtr(handle_id: StdHandle) *std.os.windows.HANDLE {
 
 fn forwardFromPipe(channel: ipc.StandardOutputChannel, pipe: std.fs.File) void {
     defer pipe.close();
-    var rdr = std.io.bufferedReader(pipe.reader());
-    var buf: std.ArrayListUnmanaged(u8) = .empty;
-    defer buf.deinit(alloc);
+    var rdr_buf: [4096]u8 = undefined;
+    var rdr = pipe.reader(&rdr_buf);
+    var buf = std.io.Writer.Allocating.initCapacity(alloc, 256) catch |e| {
+        logger.err("Error in stdio forwarder: {}", .{e});
+        return;
+    };
+    defer buf.deinit();
     while (true) {
         defer buf.clearRetainingCapacity();
 
-        rdr.reader().streamUntilDelimiter(buf.writer(alloc), '\n', null) catch |e| switch (e) {
-            error.EndOfStream => {},
-            else => {
-                logger.err("Error in stdio forwarder: {}", .{e});
-                return;
-            },
+        _ = rdr.interface.streamDelimiterEnding(&buf.writer, '\n') catch |e| {
+            logger.err("Error in stdio forwarder: {}", .{e});
+            return;
         };
-
-        if (buf.getLastOrNull()) |c| {
-            if (c == '\r') {
-                _ = buf.pop().?;
-            }
+        if (rdr.interface.buffer.len != 0) {
+            std.debug.assert(rdr.interface.buffer[0] == '\n');
+            rdr.interface.buffer = rdr.interface.buffer[1..];
         }
 
-        if (tryForwardLineAsLogRecord(buf.items)) {
+        var line = buf.getWritten();
+        if (line.len != 0 and line[line.len - 1] == '\r') {
+            line = line[0 .. line.len - 1];
+        }
+
+        if (tryForwardLineAsLogRecord(line)) {
             continue;
         }
 
         // forward stdout and stderr to our log file
-        root.logToLogFile(.info, @tagName(channel), "{s}", .{buf.items});
+        root.logToLogFile(.info, @tagName(channel), "{s}", .{line});
 
         // forward normally
-        rs.sendOutputLine(channel, buf.items);
+        rs.sendOutputLine(channel, line);
     }
 }
 

agent/build.zig.zon
@@ -5,16 +5,16 @@
 
     .fingerprint = 0x976fd25251d885f2, // Changing this has security and trust implications.
 
-    .minimum_zig_version = "0.14.1",
+    .minimum_zig_version = "0.15.0-dev.1092+d772c0627",
 
     .dependencies = .{
         .dll_proxy = .{
-            .url = "git+https://github.com/manderrow/dll-proxy.git#abfbaaec203ec9c9f1ce82bd3facaaeb00386665",
-            .hash = "dll_proxy-0.0.0-WDnws9I1AAC0lxwXH8V33yTQpVnrc6DBCeYoMXpdGok7",
+            .url = "git+https://github.com/manderrow/dll-proxy.git#c0e8fdd3507db46b353f310e75dc9f90ca3eb367",
+            .hash = "dll_proxy-0.0.0-WDnws-g1AAB5bZAf4Ef_DqeeFErKtQjmE6e8P7QoWvDS",
         },
         .wine_host_dlfcn = .{
-            .url = "git+https://github.com/manderrow/wine-host-dlfcn#17e56c0f42305eaafab586491dbb861c624c7e44",
-            .hash = "wine_host_dlfcn-0.0.0-853HMWIYAACimUnFdES3E9lhfxT6HYWEiPzgPtL4qTXk",
+            .url = "git+https://github.com/manderrow/wine-host-dlfcn.git#fea271e21e9207d02245b1a60d8032b94dbcf9af",
+            .hash = "wine_host_dlfcn-0.0.0-853HMSoYAAAjuFo7EBtyzubVsIfGjDnuEPQp0KKloYeH",
         },
     },
 

agent/src/crash.zig
@@ -29,22 +29,23 @@ pub fn panic(msg: []const u8, error_return_trace: ?*std.builtin.StackTrace, ret_
 const FormatSourceLocation = struct {
     src: std.builtin.SourceLocation,
 
-    pub fn format(self: FormatSourceLocation, comptime fmt: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
-        if (fmt.len != 0) @compileError("Unrecognized format specifier: " ++ fmt);
+    pub fn format(self: FormatSourceLocation, writer: *std.io.Writer) !void {
         const src = self.src;
         return writer.print("at {s}:{s}:{}:{}:{s}: ", .{ src.module, src.file, src.line, src.column, src.fn_name });
     }
 };
 
 pub fn crash(src: std.builtin.SourceLocation, comptime fmt: []const u8, args: anytype) noreturn {
-    std.debug.panic("{}" ++ fmt, .{FormatSourceLocation{ .src = src }} ++ args);
+    std.debug.panic("{f}" ++ fmt, .{FormatSourceLocation{ .src = src }} ++ args);
 }
 
 export fn manderrow_agent_crash(msg_ptr: [*]const u8, msg_len: usize) noreturn {
     @panic(msg_ptr[0..msg_len]);
 }
 
-fn dumpCrashReport(writer: anytype, msg: []const u8, ret_addr: ?usize) void {
+/// The writer should be unbuffered so that as much is written as possible in case of hard failure,
+/// and to slightly improve resistance against stack overflows.
+fn dumpCrashReport(writer: *std.io.Writer, msg: []const u8, ret_addr: ?usize) void {
     writer.print(
         \\{s}
         \\
@@ -72,7 +73,8 @@ fn dumpCrashReport(writer: anytype, msg: []const u8, ret_addr: ?usize) void {
         writer.print("Executable hash: error {}", .{e}) catch return;
         break :blk null;
     }) |hash| {
-        writer.print("Executable hash: {}", .{std.fmt.fmtSliceHexLower(&hash)}) catch return;
+        writer.writeAll("Executable hash: ") catch return;
+        writer.printHex(&hash, .lower) catch return;
     }
     // TODO: hash agent, host agent and host_dlfcn libraries
 }
@@ -125,7 +127,9 @@ fn reportCrashToFile(msg: []const u8, ret_addr: ?usize) void {
         ) catch return;
     }
 
-    dumpCrashReport(file.writer(), msg, ret_addr);
+    // see doc comment on dumpCrashReport for unbuffered rationale
+    var wtr = file.writer(&.{});
+    dumpCrashReport(&wtr.interface, msg, ret_addr);
 }
 
 fn reportCrashToStderr(msg: []const u8, ret_addr: ?usize) void {
@@ -135,5 +139,7 @@ fn reportCrashToStderr(msg: []const u8, ret_addr: ?usize) void {
     if (stdio.real_stderr == null) std.debug.lockStdErr();
     defer if (stdio.real_stderr == null) std.debug.unlockStdErr();
 
-    dumpCrashReport((stdio.real_stderr orelse std.io.getStdErr()).writer(), msg, ret_addr);
+    // see doc comment on dumpCrashReport for unbuffered rationale
+    var wtr = (stdio.real_stderr orelse std.fs.File.stderr()).writer(&.{});
+    dumpCrashReport(&wtr.interface, msg, ret_addr);
 }

agent/src/root.zig
@@ -47,7 +47,7 @@ fn logFn(
         .stderr => {
             std.debug.lockStdErr();
             defer std.debug.unlockStdErr();
-            logToFile(std.io.getStdErr(), level, @tagName(scope), format, args);
+            logToFile(std.fs.File.stderr(), level, @tagName(scope), format, args);
         },
     }
 }
@@ -72,8 +72,11 @@ pub fn logToFile(
     comptime format: []const u8,
     args: anytype,
 ) void {
-    file.writer().print("{s} {s} ", .{ @tagName(message_level), scope }) catch return;
-    file.writer().print(format ++ "\n", args) catch return;
+    var file_buf: [4096]u8 = undefined;
+    var wtr = file.writer(&file_buf);
+    defer wtr.interface.flush() catch {};
+    wtr.interface.print("{s} {s} ", .{ @tagName(message_level), scope }) catch return;
+    wtr.interface.print(format ++ "\n", args) catch return;
 }
 
 pub const logger = std.log.scoped(.manderrow_agent);
@@ -91,7 +94,7 @@ comptime {
                 _ = windows;
             },
             .macos => {
-                @export(&[1]*const fn () callconv(.C) void{entrypoint_macos}, .{
+                @export(&[1]*const fn () callconv(.c) void{entrypoint_macos}, .{
                     .section = "__DATA,__mod_init_func",
                     .name = "init_array",
                 });
@@ -101,7 +104,7 @@ comptime {
                     @export(&[1]*const fn (
                         argc: c_int,
                         argv: [*][*:0]u8,
-                    ) callconv(.C) void{entrypoint_linux_gnu}, .{
+                    ) callconv(.c) void{entrypoint_linux_gnu}, .{
                         .section = ".init_array",
                         .name = "init_array",
                     });
@@ -146,8 +149,8 @@ pub fn entrypoint(module: if (builtin.os.tag == .windows) std.os.windows.HMODULE
 
     logger.debug("Attached segfault handler", .{});
 
-    logger.debug("{}", .{dump_args});
-    logger.debug("{}", .{dump_env});
+    logger.debug("{f}", .{dump_args});
+    logger.debug("{f}", .{dump_env});
 
     startAgent();
 
@@ -180,8 +183,8 @@ fn startAgent() void {
 
     logger.debug("Parsed arguments", .{});
 
-    logger.debug("{}", .{dump_args});
-    logger.debug("{}", .{dump_env});
+    logger.debug("{f}", .{dump_args});
+    logger.debug("{f}", .{dump_env});
 
     switch (build_options.ipc_mode) {
         .ipc_channel, .winelib => |t| {
@@ -207,28 +210,28 @@ fn startAgent() void {
 
     logger.info("Agent started", .{});
     {
-        var buf: std.ArrayListUnmanaged(u8) = .empty;
-        defer buf.deinit(alloc);
+        var buf = std.io.Writer.Allocating.init(alloc);
+        defer buf.deinit();
 
-        dumpArgs(buf.writer(alloc)) catch {};
+        dumpArgs(&buf.writer) catch {};
         switch (build_options.ipc_mode) {
             .ipc_channel, .winelib => {
-                rs.sendLog(.debug, "manderrow_agent", buf.items) catch |e| logger.warn("{}", .{e});
+                rs.sendLog(.debug, "manderrow_agent", buf.getWritten()) catch |e| logger.warn("{}", .{e});
             },
             .stderr => {
-                logger.debug("{s}", .{buf.items});
+                logger.debug("{s}", .{buf.getWritten()});
             },
         }
 
         buf.clearRetainingCapacity();
 
-        dumpEnv(buf.writer(alloc)) catch {};
+        dumpEnv(&buf.writer) catch {};
         switch (build_options.ipc_mode) {
             .ipc_channel, .winelib => {
-                rs.sendLog(.debug, "manderrow_agent", buf.items) catch |e| logger.warn("{}", .{e});
+                rs.sendLog(.debug, "manderrow_agent", buf.getWritten()) catch |e| logger.warn("{}", .{e});
             },
             .stderr => {
-                logger.debug("{s}", .{buf.items});
+                logger.debug("{s}", .{buf.getWritten()});
             },
         }
     }
@@ -300,7 +303,7 @@ fn interpret_instructions(instructions: []const Args.Instruction) void {
     for (instructions) |insn| {
         switch (insn) {
             .load_library => |ll| {
-                logger.debug("Loading library from \"{}\"", .{std.zig.fmtEscapes(ll.path)});
+                logger.debug("Loading library from \"{f}\"", .{std.zig.fmtString(ll.path)});
                 switch (builtin.os.tag) {
                     .windows => {
                         const buf = path_buf orelse blk: {
@@ -331,7 +334,7 @@ fn interpret_instructions(instructions: []const Args.Instruction) void {
             .set_var => |sv| {
                 const key = sv.kv[0..sv.eq_sign];
                 const value = sv.kv[sv.eq_sign + 1 .. :0];
-                logger.debug("Setting environment variable {s}=\"{}\"", .{ key, std.zig.fmtEscapes(value) });
+                logger.debug("Setting environment variable {s}=\"{f}\"", .{ key, std.zig.fmtString(value) });
                 switch (builtin.os.tag) {
                     .windows => {
                         const key_buf = std.unicode.wtf8ToWtf16LeAllocZ(alloc, key) catch |e| switch (e) {
@@ -365,20 +368,18 @@ fn interpret_instructions(instructions: []const Args.Instruction) void {
 }
 
 const dump_args = struct {
-    pub fn format(_: @This(), comptime fmt: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
-        if (fmt.len != 0) @compileError("Unsupported format specifier: " ++ fmt);
+    pub fn format(_: @This(), writer: *std.io.Writer) !void {
         dumpArgs(writer) catch {};
     }
 }{};
 
 const dump_env = struct {
-    pub fn format(_: @This(), comptime fmt: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
-        if (fmt.len != 0) @compileError("Unsupported format specifier: " ++ fmt);
+    pub fn format(_: @This(), writer: *std.io.Writer) !void {
         dumpEnv(writer) catch {};
     }
 }{};
 
-fn dumpArgs(writer: anytype) !void {
+fn dumpArgs(writer: *std.io.Writer) !void {
     try writer.writeAll("Args:");
     var iter = std.process.argsWithAllocator(alloc) catch |e| {
         try writer.writeAll("Failed to get args (Out of memory)");
@@ -386,7 +387,7 @@ fn dumpArgs(writer: anytype) !void {
     };
     defer iter.deinit();
     while (iter.next()) |arg| {
-        try writer.print(" \"{}\"", .{std.zig.fmtEscapes(arg)});
+        try writer.print(" \"{f}\"", .{std.zig.fmtString(arg)});
     }
 }
 
@@ -399,7 +400,7 @@ fn dumpEnv(writer: anytype) !void {
     defer map.deinit();
     var iter = map.iterator();
     while (iter.next()) |entry| {
-        try writer.print("  {s}=\"{}\"\n", .{ entry.key_ptr.*, std.zig.fmtEscapes(entry.value_ptr.*) });
+        try writer.print("  {s}=\"{f}\"\n", .{ entry.key_ptr.*, std.zig.fmtString(entry.value_ptr.*) });
     }
     try writer.writeAll("}");
 }

agent/src/rs/winelib.zig
@@ -60,7 +60,7 @@ pub fn init(host_dlfcn_lib_path: [:0]const u16, host_lib_path: [:0]const u8) voi
     logger.debug("Loading host library", .{});
 
     const dlfcns = dlfcn.init(host_dlfcn_lib_path) catch |e| {
-        std.debug.panic("Failed to load host_dlfcn library from {}: {s}", .{ std.unicode.fmtUtf16Le(host_dlfcn_lib_path), @errorName(e) });
+        std.debug.panic("Failed to load host_dlfcn library from {f}: {s}", .{ std.unicode.fmtUtf16Le(host_dlfcn_lib_path), @errorName(e) });
     };
 
     logger.debug("Loaded host_dlfcn library", .{});

agent/src/util/windows.zig
@@ -16,7 +16,7 @@ pub fn panicWindowsError(src: std.builtin.SourceLocation, func: []const u8) nore
         buf_wstr.len,
         null,
     );
-    crash.crash(src, "error.Unexpected(0x{x}): {s}: {}\n", .{
+    crash.crash(src, "error.Unexpected(0x{x}): {s}: {f}\n", .{
         @intFromEnum(err),
         func,
         std.unicode.fmtUtf16Le(buf_wstr[0..len]),

commit b0974553e9d113740e530dfc5a5928b9b9dd6b72

agent: Fix bug introduced in 44a10168a319bdea292d5763a55de1c23e1a4530

agent/src/stdio.zig
@@ -94,9 +94,9 @@ fn forwardFromPipe(channel: ipc.StandardOutputChannel, pipe: std.fs.File) void {
             logger.err("Error in stdio forwarder: {}", .{e});
             return;
         };
-        if (rdr.interface.buffer.len != 0) {
-            std.debug.assert(rdr.interface.buffer[0] == '\n');
-            rdr.interface.buffer = rdr.interface.buffer[1..];
+        if (rdr.interface.bufferedLen() != 0) {
+            std.debug.assert(rdr.interface.buffered()[0] == '\n');
+            rdr.interface.seek += 1;
         }
 
         var line = buf.getWritten();