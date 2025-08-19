19d36cfa48f8268b22dc2485f738922c60fdbf0c
Jack <canadajackhuang@gmail.com>
2025-08-19 18:33:43 -0400

    Add logo to splashscreen

--- src/pages/splashscreen/Splashscreen.tsx
+++ src/pages/splashscreen/Splashscreen.tsx
@@ -1,4 +1,5 @@
 import styles from "./Splashscreen.module.css";
+import logo from "../../assets/Manderrow logo.svg";
 
 export default function Splashscreen() {
   return (
@@ -6,7 +7,7 @@ export default function Splashscreen() {
       <h1 class={styles.splashscreen__title}>Manderrow</h1>
       <p class={styles.splashscreen__text}>Launching...</p>
       <div class={styles.splashscreen__loader} aria-hidden></div>
-      {/* <img src="/img/logo512.png" alt="Logo" class="splashscreen__logo-image" /> */}
+      <img src={logo} alt="Manderrow logo" class={styles.splashscreen__logo} />
     </main>
   );
 }
