78cd73bb8453cad9a4e5d37dc6c602472d3a4f49
Michael Pfaff <michael@pfaff.dev>
2025-08-29 00:29:49 -0400

    Fix card offset calculation and move calculation into CSS

--- src/components/profile/ModList.tsx
+++ src/components/profile/ModList.tsx
@@ -508,18 +508,8 @@ function ModUpdateDialogue(props: { onDismiss: () => void; updates: ModUpdate[] 
 }
 
 const MAX_SELECTED_CARDS = 5;
-const CARD_WINDOW_DEG = 120;
-const DEG_TO_PERCENT = 100 / 360;
 function SelectedModsList(props: { mods: Accessor<Map<ModId, string>> }) {
   const selectedCount = () => props.mods().size;
-
-  // the spacing between each card
-  const spacing = 180 / (selectedCount() + 1);
-  // the degrees of padding to fit the window
-  const padding = (180 - CARD_WINDOW_DEG) / 2;
-  // the base offset of each card
-  // You may need to add a multiple of 90. Idk where the origin is by default.
-  const offset = padding + spacing / 2;
 
   return (
     <>
@@ -527,21 +517,16 @@ function SelectedModsList(props: { mods: Accessor<Map<ModId, string>> }) {
         <ul
           class={styles.cards__list}
           style={{
-            "--card-divisions": Math.min(MAX_SELECTED_CARDS, selectedCount() + 1),
-            "--total-cards": Math.min(MAX_SELECTED_CARDS, selectedCount()),
+            "--total-cards": Math.min(selectedCount(), MAX_SELECTED_CARDS),
           }}
         >
           <For each={Array.from(props.mods()).slice(0, MAX_SELECTED_CARDS)}>
             {([modId, version], i) => {
-              // the degree offset of each card from the origin on the circle
-              const angle = offset + spacing * i();
-              const offsetDistance = angle * DEG_TO_PERCENT - 50;
-
               return (
                 <li
                   class={styles.selected__card}
                   style={{
-                    "--offsetDistance": `${offsetDistance}%`,
+                    "--card-index": i(),
                     "--bg-image": `url("${getIconUrl(modId.owner, modId.name, version)}")`,
                   }}
                 >
